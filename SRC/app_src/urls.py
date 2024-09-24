bc222cc43f60c52b180a36241283cbae831e5a99
rvjq <rvjq@cesar.school>
2024-09-24 19:59:38 -0300

    Added logout function

--- SRC/app_src/urls.py
+++ SRC/app_src/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
+    path('plataforma', views.plataforma, name='plataforma'),
 ]
--- SRC/auth_src/urls.py
+++ SRC/auth_src/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup', views.signup, name='signup'),
-    path('plataforma', views.plataforma, name='plataforma'),
     path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
