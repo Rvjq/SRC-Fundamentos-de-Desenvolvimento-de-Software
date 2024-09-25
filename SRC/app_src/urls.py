b2ec806584ce8d62bc8ec86b9a3f4d0b8d7bf845
rvjq <rvjq@cesar.school>
2024-09-25 16:21:30 -0300

    Adicionado historia 3

--- SRC/app_src/urls.py
+++ SRC/app_src/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
     path('clientes/create', views.clientes_create, name='clientes_create'),
+    path('clientes/delete/<int:id>/', views.clientes_delete, name='clientes_delete'),
+    path('clientes/edit/<int:id>/', views.clientes_edit, name='clientes_edit'),
     path('plataforma', views.plataforma, name='plataforma'),
 ]
