1ea3add4a73c113476ee3ac8764409e9a9042be9
rvjq <rvjq@cesar.school>
2024-09-24 21:40:57 -0300

    model for client

--- SRC/app_src/models.py
+++ SRC/app_src/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import random
 
-# Create your models here.
+def generate_unique_ra():
+    while True:
+        ra = str(random.randint(10**9, 10**10 - 1))  # Gera um RA de 10 dígitos
+        if not client.objects.filter(ra=ra).exists():
+            return ra
+
+class client (models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email= models.EmailField(null=True, max_length=254)
+    ra = models.CharField(max_length=10, unique=True, default=generate_unique_ra)
+    
+    def __str__(self):
+        return self.usuario.username
